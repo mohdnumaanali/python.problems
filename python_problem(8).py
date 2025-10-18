5333069f25cb3744d4042891d27e354f0397bfa5
mohdnumaanali <mohammednumaanalins@gmail.com>
2025-10-18 15:40:55 +0530

    today  codes
    new task new day

---
+++ python_problem(8).py
@@ -0,0 +1,8 @@
+pet = True
+input("what is your pet: ")
+survey = input(" is your pet  is aggressive (yes/no): ").strip().lower()
+
+if survey == "no":
+      print("allowed")
+else:
+    print("not allowed ")
