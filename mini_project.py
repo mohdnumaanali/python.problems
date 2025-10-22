529f63bbd5f1cf179929309f00be7b4f7eb7f920
mohdnumaanali <mohammednumaanalins@gmail.com>
2025-10-22 06:24:14 +0530

    something new
    i have change few things in my code

---
+++ mini_project.py
@@ -0,0 +1,2 @@
+name = input(" what is your name : ")
+print("hello"  + name +  "\n welcome to this world "+ name)
--- python_problem(6).py
+++ python_problem(6).py
@@ -1,9 +1,11 @@
 #here we are going to study how to add any name or something 
-
+'''
 name = "Ali "
-print ("hello "+ name )
+print ("hello "+ name )'''
 
 # from this i have modified some things here 
 
-name2 = input(" give any input here :  ")
-print("welcome to this world "+ name2)
+name2 = input(" what is your name :  ")
+print("\n welcome to this world "+ name2)
+print("\n what are you doing "+name2)
+reply = input(": ")
