f88979707120c89176f4ffda0fadaf2e989c9e84
mohdnumaanali <mohammednumaanalins@gmail.com>
2025-11-08 22:55:37 +0530

    Create python_problem(21).py

---
+++ python_problem(21).py
@@ -0,0 +1,5 @@
+#loop and upper 
+roll = "ab12"
+for ch in roll.upper():
+    print(ch, end=" ")   #agar mai space nahi dera toh space nahi ara code me so delo ("")  iska output AB12 aya 
+                        #agar mai space dera toh space ara code mai jaise (" ")  iska output A B 1 2 aya  
