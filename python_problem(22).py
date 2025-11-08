5eaf482a8510fbf5416f8e6b89cb52817d6dfbc0
mohdnumaanali <mohammednumaanalins@gmail.com>
2025-11-09 00:02:01 +0530

    new work

---
+++ python_problem(22).py
@@ -0,0 +1,14 @@
+#if - else question 
+price = 150 
+if price > 100 :
+    print("discout is applied ")        #yai code mai hum if and else kaisa use hora wo baata rahai hai so you can check 
+else :
+    print(" no discout ")
+
+# by user 
+price_str=input(" what is your amount = ")      #bhai code toh run hogaya par mujai doubt hai baad mai clear lengai 
+price = int(price_str)
+if price > 100 :
+    print("Discout is applied ")
+else:
+    print("no Discout ")
---
+++ python_problem(23).py
@@ -0,0 +1,26 @@
+#elif     
+city = " delhi "
+
+if city == "mumbai ":
+    print("wester route ")
+elif city  == " delhi " :
+    print("northern route ")        #this code is elif ladder questions nice example ..  assaan hai bhai 
+elif city == "hyderabad":
+    print("southern india ")
+else:
+    print("other route ")
+
+# customizing by my own 
+
+place = input("where you live : ")
+
+if place == "north": 
+    print("bollywood ")
+elif place == "south":
+    print("tollywood ")
+elif place == "tamil":
+    print ("mollywood")
+elif place == "karnataka":
+    print("kollywood")
+else :
+    print(" hollywood ")
